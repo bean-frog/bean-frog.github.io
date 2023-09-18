7617563350dbef09b87cdef2b4554feafbda8ebf
bean-frog <gkieran32855@gmail.com>
2023-09-18 14:19:36 -0700

    more stuff

--- blog/posts.js
+++ blog/posts.js
@@ -2,8 +2,8 @@ const posts = [
 {
     "id":"1",
     "title":"First Post: Hello World!",
-    "date":"date 1",
-    "content":"goofy ah content goes here it is definitely very good and relevant and is not completely random"
+    "date":"18 Sep 2023",
+    "content":"Hello World!! This is the first post on my new blog. Hopefully this place won't turn into a dumpster fire. <br> Today I also added some new splash texts to the homepage. pretty interesting I guess."
 }
 ]
 
@@ -12,7 +12,7 @@ posts.forEach((post) => {
       <article id="${post.id}" class="searchable bg-white rounded-lg shadow p-4 mb-8">
         <span><div class="badge badge-outline text-custom">${post.date}</div></span>
         <h2 class="title text-2xl font-bold mb-4 text-custom">${post.title}</h2>
-        <p class="text-custom">${post.content}</p>
+        <p class="text-custom prose">${post.content}</p>
       </article>
     `;
   
