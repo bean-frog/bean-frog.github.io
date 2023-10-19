58725cdb2909a73703c8585933e0ecafed061329
bean-frog <gkieran32855@gmail.com>
2023-10-19 11:51:45 -0700

    move splash text array to one file

---
+++ splashtexts.js
@@ -0,0 +1,32 @@
+const balls = [
+    "I do things, just not very well",
+    "mid developer lol",
+    "Tailwind enjoyer",
+    "404: brain not found",
+    "do it for ken",
+    "#kb43ver",
+    "Go donate to 43i.org",
+    "Red Bull is overrated",
+    "My dog has 1 braincell (that's still more than me)",
+    "I purchase uncles in bulk",
+    "kys (keep yourself safe) :D",
+    "W gyatt kai cenat twitch stream rizz? Only in ohio skibidi toilet w gyatt baby gronk???!!?!?",
+    "You just lost the game!",
+    "Man who made it must have been a CHAMP",
+    "A Who? A What? A LERODGE!",
+    "You're telling me a SHRIMP fried this rice????",
+    "Top 10 best waterslides",
+    "Moco de Gorila goes hard",
+    "NI!",
+    "BRING ME A SHRUBBERY!",
+    "What is the airspeed velocity of an unladen swallow?",
+    "Life.... is Roblox",
+    "Tell 'em to bring out the whole ocean!",
+    "Back to the lab again, yo",
+    "*aggressive vine boom*",
+    "one must imagine bean_frog happy",
+    "luh calm fit nun too extravagant nun too rambunctious",
+    "DIS MY BROTHER BILO BTW"
+    
+ 
+  ];
