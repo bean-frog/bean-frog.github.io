1f3a68c2ab2cf71618d009496f396a09829f19ba
bean-frog <gkieran32855@gmail.com>
2023-12-17 20:32:06 -0800

    womp womp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: ["./index.html","./funstuff.html", "./content.js"],
+  content: ["./index.html","./funstuff.html", "./content.js", "./portfolio/index.html", "./portfolio/index.js"],
   theme: {
     extend: {},
   },
